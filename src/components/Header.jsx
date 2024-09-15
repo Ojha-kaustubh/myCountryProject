4ff7a9c443e82b1da266adb92b4fa80066214e20
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2024-09-15 15:21:20 +0530

    created the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,19 @@ import React from 'react'
 export const Header = () => {
   return (
 
-    <>
-    <h1> Hello World</h1>
-    </>
+    <header className="header-container">
+      <div className="header-content">
+        <div className="tittle">
+          <h2>
+          <a href='/'>Where is My Country</a>
+          </h2>
+        </div>
+        <p className="theme-changer">
+          <i className="fa-regular fa-moon" />
+          &nbsp;&nbsp;Dark Mode
+        </p>
+      </div>
+    </header>
 
     )
 }
