ebb5f53e88f7b49fa842b8c2a9f8814c257c2e5a
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-18 23:35:45 +0530

    create the country Card compenent

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,27 +1,22 @@
 export default function CountryCard({ country }) {
-  //   const { name, flags, population, region, capital } = country;
-
-  return (
-    <a
-      className="country-card"
-      href={`/country.html?name=${country.name.common}`}
-    >
-      <img src={country.flag.svg} alt={country.name.common + "Flag"} />
-      <div className="card-text">
-        <h3 className="card-title">{country.name.common}</h3>
-        <p>
-          <b>Population :</b> {country.population.toLocaleString("en-IN")}
-        </p>
-
-        <p>
-          <b>Region: </b>
-          {country.region}
-        </p>
-        <p>
-          <b>Capital: </b>
-          {country.capital?.[0]}
-        </p>
-      </div>
-    </a>
-  );
-}
+    return (
+      <a
+        className="country-card"
+        href={`/country.html?name=${country.name.common}`}
+      >
+        <img src={country.flags.svg} alt={country.name.common + " Flag"} />
+        <div className="card-text">
+          <h3 className="card-title">{country.name.common}</h3>
+          <p>
+            <b>Population:</b> {country.population.toLocaleString("en-IN")}
+          </p>
+          <p>
+            <b>Region:</b> {country.region}
+          </p>
+          <p>
+            <b>Capital:</b> {country.capital?.[0]}
+          </p>
+        </div>
+      </a>
+    );
+  }
